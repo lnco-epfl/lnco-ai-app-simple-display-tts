86e1e1c68e8ad5838b55f8521a0fd4b71da7d915
Yannick Mijsters <gerardus.mijsters@epfl.ch>
2026-05-29 16:11:48 +0200

    feat: add audio files

--- src/config/audio.ts
+++ src/config/audio.ts
@@ -4,5 +4,8 @@ export type PreloadedAudio = {
 };
 
 export const PRELOADED_AUDIO: PreloadedAudio[] = [
-  // { label: 'Introduction', src: '/assets/audio/intro.mp3' },
+  { label: 'Intro screen 1', src: '/assets/audio/intro-screen-1.mp3' },
+  { label: 'Intro screen 2', src: '/assets/audio/intro-screen-2.mp3' },
+  { label: 'Intro screen 3', src: '/assets/audio/intro-screen-3.mp3' },
+  { label: 'Break 5 minutes', src: '/assets/audio/break-5-minutes.mp3' },
 ];
